ce357fdc70d3bf3cd7248049cf7f1915984036c9
Zach <zachlandis91@gmail.com>
2022-10-24 13:18:41 -0600

    Green headers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content aded by JavaScript";
+h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
